0f7d0bc72fd791332012fafad89e952d924ed924
Andrew Tzeng <Andrew Tzeng>
2019-11-03 14:55:14 -0500

    Added a mysql docker container in the compose file, removed redis, added graphql functionality to app

--- first_docker_flask_app/app.py
+++ first_docker_flask_app/app.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template
 import random
 import time
-import redis
+import graphene
+from flask_graphql import GraphQLView
+from models import db_session
+from schema import schema, Department
 
 app = Flask(__name__)
-cache = redis.Redis(host="redis", port=6379)
+app.debug = True
+
+app.add_url_rule(
+    '/graphql',
+    view_func= GraphQLView.as_view(
+        'graphql',
+        schema = schema,
+        graphiql=True
+    )
+)
 
 @app.route('/')
 def index():
-    text = 'You hit the index page'
-    count = get_hit_count()
-    count_text = 'Hello, you have been seen {} times.\n'.format(count)
+    text = 'You hit the index function'
+    count_text = 'You hit this at some num of times'
     return render_template('index.html', text=text, count=count_text)
 
 @app.route('/test')
 def test():
     return render_template('index.html',text = 'testing 1 2 3', count='Count says what?')
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -=1
-            time.sleep(0.5)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
---
+++ first_docker_flask_app/schema.py
@@ -0,0 +1,36 @@
+import graphene
+from graphene import relay
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from models import db_session, Department as DepartmentModel, Employee as EmployeeModel
+
+
+class Department(SQLAlchemyObjectType):
+    class Meta:
+        model = DepartmentModel
+        interfaces = (relay.Node, )
+
+
+class DepartmentConn(relay.Connection):
+    class Meta:
+        node = Department
+
+
+class Employee(SQLAlchemyObjectType):
+    class Meta:
+        model = EmployeeModel
+        interfaces = (relay.Node, )
+
+
+class EmployeeConn(relay.Connection):
+    class Meta:
+        node = Employee
+
+
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+    # Allows sorting over multiple columns, by default over the primary key
+    all_employees = SQLAlchemyConnectionField(EmployeeConn)
+    # Disable sorting over this field
+    all_departments = SQLAlchemyConnectionField(DepartmentConn, sort=None)
+
+schema = graphene.Schema(query=Query)
